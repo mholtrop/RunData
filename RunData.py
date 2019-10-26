7bf0140b6b8ced52055534a5407f9778ee8b30e2
Rafayel Paremuzyan <rafopar@jlab.org>
2019-10-25 21:10:33 -0400

    The most recent trigger file was missing from the Good_trigger list. Not it is added

--- RunData.py
+++ RunData.py
@@ -795,7 +795,7 @@ if __name__ == "__main__":
     data.Good_triggers=['hps_v7.cnf','hps_v8.cnf','hps_v9.cnf','hps_v9_1.cnf',
                         'hps_v9_2.cnf','hps_v10.cnf',
                         'hps_v11_1.cnf','hps_v11_2.cnf','hps_v11_3.cnf','hps_v11_4.cnf',
-                        'hps_v11_5.cnf','hps_v11_6.cnf']
+                        'hps_v11_5.cnf','hps_v11_6.cnf','hps_v12_1.cnf' ]
     data.Production_run_type=["PROD66","PROD67"]
     data.target_dict = HPS_2019_Run_Target_Thickness()
 
