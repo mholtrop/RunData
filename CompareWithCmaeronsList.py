ad26af64cbe3dbeff7a3d53e34f163b9b77eebd7
Rafayel Paremuzyan <rafopar@jlab.org>
2019-10-27 19:29:23 -0400

    A simple code that compares, Maurik's and Cameron's lists, and prints list of runs that are common in both lists, and runs that exist in one, and doesn't in another

---
+++ CompareWithCmaeronsList.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import re
+
+def CompareLists(listA, listB):
+    missinginA = []
+    missinginB = []
+
+    PresentinBoth = []
+
+    for runA in listA:
+        if runA in listB:
+            PresentinBoth.append(runA)
+        else:
+            missinginB.append(runA)
+
+    for runB in listB:
+        if runB in listA:
+            continue
+        else:
+            missinginA.append(runB)
+
+    CompareDict = dict()
+    CompareDict["missinginA"] = missinginA
+    CompareDict["missinginB"] = missinginB
+    CompareDict["PresentinBoth"] = PresentinBoth
+
+    return CompareDict
+
+
+
+
+
+MauriksList = []
+CameronsList = []
+
+with open("Runs2019_withCharge.dat") as ff:
+    for line in ff:
+        line = line.replace('\n', '')
+        MauriksList.append(line)
+
+
+with open("CameronsList.dat") as ff:
+    for line in ff:
+        line = line.replace('\n', '')
+        CameronsList.append(line)
+
+
+compareResult = CompareLists(MauriksList, CameronsList)
+
+print (" =============================== Maurik's List ===================================== ")
+print (MauriksList)
+
+print (" =============================== Cameron's List ===================================== ")
+print (CameronsList)
+
+
+print ("=============================== Runs in both list ====================================")
+print (compareResult["PresentinBoth"])
+
+
+print ("=============================== Missing in Maurik's list ====================================")
+print (compareResult["missinginA"])
+
+
+print ("=============================== Missing in Cameron's list ====================================")
+print (compareResult["missinginB"])
